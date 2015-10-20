f6f5c20183f12fd5a729f714b0ec9d516e4cad58
Louis Wilbrink <louis.wilbrink@gmail.com>
2015-10-20 08:49:22 -0600

    add gulp sass task; consolidated location of scss and css; updated index.html with new css paths

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,30 @@
 'use strict';
 
 var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
+var plugins = require('gulp-load-plugins')(); // See package.json for plugins.
 var argv = require('yargs').argv;
 var fs = require('fs');
+var util = require('util');
 
+// Starts the node application
 function start () {
     return gulp.src('')
         .pipe(plugins.shell(['node start.js']));
 }
+
+/* 
+ * This task will compile the application .scss files into .css files and 
+ * save them in the `src/app/styles/` folder.  Note that each component 
+ * contains its own .scss file.  
+ */
+gulp.task('sass', function () {
+    return gulp.src([
+        'src/app/styles/scss/*.scss',
+        'src/app/components/**/*.scss'
+    ])
+    .pipe(plugins.sass())
+    .pipe(gulp.dest('src/app/styles/css'));
+});
 
 gulp.task('start', function () {
     return start();
